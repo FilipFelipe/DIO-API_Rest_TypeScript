619bacb3145470b60993c83c99b28f263eaa6c61
Filip Felipe <filipfelipe@outlook.com>
2022-04-20 10:06:27 -0300

    Update
    - Configuração do Express;
    - Configuração para automatização com 'ts-node-dev'

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world')
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express();
+const PORT = 3000;
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'bar1' })
+});
+
+app.listen(PORT, () => {
+    console.log(`API executando na porta ${PORT}`);
+});
